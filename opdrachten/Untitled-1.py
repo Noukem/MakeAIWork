9fb6d3d9f5802b168d5b117fa6787ada6ab77da9
Noukem <anoukcbroekhuizen@gmail.com>
2023-02-09 11:03:54 +0100

    live opdrachten

---
+++ opdrachten/Untitled-1.py
@@ -0,0 +1,27 @@
+#Define the number eleven
+number = 11 
+
+#As long as result is larger than zero
+while number > 0:
+
+#Subtract three
+    number = number - 3
+
+# Check if is zero or no​t
+#print result​
+if number == 0:
+    print("divisible")
+
+else: 
+    print("nondivisible")
+
+
+
+    
+
+
+    
+
+
+
+
